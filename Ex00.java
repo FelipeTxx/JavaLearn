0bf97e1d23908f7a729374e307de765520c004ae
FelipeTxx <feliperemes1@gmail.com>
2025-04-02 22:03:48 -0300

    s

--- Ex00.java
+++ Ex00.java
@@ -1,19 +1,8 @@
-import java.util.Scanner;
-import java.util.ArrayList;
 class Ex00{
-    
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        ArrayList<Double> 
-        int qnums = scanner.nextInt();
-        String nums = scanner.nextLine();
-        nums = scanner.nextLine();
-        String[] arrayNums = nums.split(" ");
-        for (int i = qnums;i>0;i--){
-            Math.sqrt(arrayNums[i]);
-        }
-
-
-        scanner.close();
+        System.out.println("hello world!");
+        System.out.println("1º  TADS 2025");
+        System.out.println("O comando System.out.println permite que sejam exibidas mensagens do sistema definidas pelo programador para o usuario");
+        
     }
 }
--- Ex01.java
+++ Ex01.java
@@ -1,8 +1,6 @@
 class Ex01{
     public static void main(String[] args) {
-        System.out.println("hello world!");
-        System.out.println("1º  TADS 2025");
-        System.out.println("O comando System.out.println permite que sejam exibidas mensagens do sistema definidas pelo programador para o usuario");
         
+
     }
 }
